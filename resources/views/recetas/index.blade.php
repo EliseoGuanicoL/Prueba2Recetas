5b79f8d42760114d71ff3adee36f549ad3d342d8
EliseoGuanicoL <eliseo_guanico.l@outlook.com>
2020-12-08 19:02:56 -0600

    Probando actualizaciones en nuevos repositorios.

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -8,18 +8,18 @@
 @section('content')
 <h2 class="text-center mb-5">Administra tus recetas</h2>
 
-    <div class="col-md-10 mx-auto bg-white p-3">
-        <table class="table">
-            <thead class="bg-primary text-light">
-                <tr>
-                    <th scole="col">Titulo</th>
-                    <th scole="col">Categoria</th>
-                    <th scole="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
+<div class="col-md-10 mx-auto bg-white p-3">
+    <table class="table">
+        <thead class="bg-primary text-light">
+        <tr>
+            <th scole="col">Titulooo</th>
+            <th scole="col">Categoria</th>
+            <th scole="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
 
-                @foreach($recetas as $receta)
+        @foreach($recetas as $receta)
 
                 <tr>
                     <td>{{ $receta->titulo }}</td>
